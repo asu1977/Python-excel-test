45633e04fa1781df258498bf52ef5c362baae346
asu1977 <asu1977@gmail.com>
2017-04-03 11:43:04 +0300

    Refactor using Rope

--- phone_excel.py
+++ phone_excel.py
@@ -1,37 +1,37 @@
 import xlrd
 
 
-a = ''
+str_first_name = ''
 
 def main():
     start()
 
 def start():
-    a = input('Введите фамилию или q для выхода из программы\n')
-    if a == 'q':
+    str_first_name = input('Введите фамилию или q для выхода из программы\n')
+    if str_first_name == 'q':
         exit(0)
 
-    a = a.title()
+    str_first_name = str_first_name.title()
 
-    if a != '':
-        catalogue(a)
-    elif a == '0':
-        catalogue(a)
+    if str_first_name != '':
+        catalogue(str_first_name)
+    elif str_first_name == '0':
+        catalogue(str_first_name)
     else:
         pass
 
-def catalogue(a):
+def catalogue(str_first_name):
         rb = xlrd.open_workbook(r'C:\Список телефонов.xls')
         sheet = rb.sheet_by_index(0)
         vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
         for kabinet, otdel, gorod, phone, dolgnost, first_name in vals:
             first_name = first_name.strip()
-            if a == first_name[:1] or a == first_name[:2] or a == first_name[:3]:
+            if str_first_name == first_name[:1] or str_first_name == first_name[:2] or str_first_name == first_name[:3]:
                 kabinet = str(kabinet)
                 kabinet = kabinet[:3]
                 name = first_name, kabinet, otdel, gorod, int(phone), dolgnost
                 print('{} : каб. {} : {} : {} : т. {} : {}'.format(*name))
-            elif a != first_name:
+            elif first_name != str_first_name:
                 pass
 
 while True:
